a553cccc05933a0c74f6e5c383ed8639d9aa9c59
marcos.castro <marcos.castro@f13.email>
2022-12-08 03:23:27 -0300

    preview to clients

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -18,7 +18,8 @@ export class ToolbarComponent implements OnInit {
     if(redirect=='boloes'){
       this.route.navigate(['home']);
       setTimeout(()=>{
-        element.scrollY = 0;
+        element = document.getElementById("linhaTopo");
+        element.scrollIntoView({behavior: "smooth"});
         // element = document.getElementById("boloes");
         // element.scrollIntoView({behavior: "smooth"}) - window.innerHeight/2;
         // let y = window.scrollY - window.innerHeight/2;
@@ -32,8 +33,6 @@ export class ToolbarComponent implements OnInit {
         element = document.getElementById("about");
         element.scrollIntoView({behavior: "smooth"});
       }, 500);
-      let y = window.scrollY - window.innerHeight/2;
-      window.scroll(0, y);
     }else if(redirect=='contato'){
       this.route.navigate(['home']);
       setTimeout(()=>{
