edbf12d19517b34834ac6be706fcdad9432cc81e
marcos.castro <marcos.castro@f13.email>
2022-12-08 01:37:01 -0300

    add stylesheet to card of boloes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,22 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
-import { enumCardName } from 'src/app/model/card.model';
+import { CardModel, enumCardName } from 'src/app/model/card.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent {
 
-  @Input() card: any;
+  @Input() card: CardModel;
   public icons = {calendar: faCalendarCheck, clock: faClock, award: faClock};
   constructor() { }
 
-  ngOnInit(): void {
-    // if(this.card.){
+  getClass(cssClass: string){
+    if(cssClass == enumCardName.MEGA_SENA || cssClass == enumCardName.MEGA_DA_VIRADA){
+      return 'mega';
+    }else if(cssClass == enumCardName.LOTO_FACIL){
+      return 'lotofacil';
+    }else if(cssClass == enumCardName.QUINA){
+      return 'quina';
+    }else if(cssClass == enumCardName.LOTO_MANIA){
+      return 'lotomania';
+    }else if(cssClass == enumCardName.TIME_MANIA){
+      return 'timemania';
+    }else if(cssClass == enumCardName.DUPLA_SENA){
 
-    // }
+    }else if(cssClass == enumCardName.FEDERAL){
+
+    }else if(cssClass == enumCardName.LOTECA){
+
+    }else if(cssClass == enumCardName.DIA_DE_SORTE){
+
+    }else if(cssClass == enumCardName.SUPER_SETE){
+
+    }
+    else{
+      return 'milionaria';
+    }
   }
 }
--- src/app/components/form-send-message/form-send-message.component.ts
+++ src/app/components/form-send-message/form-send-message.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastService } from 'src/app/service/toast/toast.service';
 
 @Component({
   selector: 'app-form-send-message',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormSendMessageComponent implements OnInit {
 
-  constructor() { }
+  public contatoForm = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    assunto: new FormControl('', Validators.required),
+    mensagem: new FormControl('', Validators.required)
+  });
+  constructor(
+    private toastr: ToastService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  submit(){
+    console.log(this.contatoForm);
+
+    if(this.contatoForm.valid){
+    // this.toastr.success('Formulário enviado!');
+    this.toastr.error('Desculpe pelo transtorno. Sua dúvida não foi registrada, por favor entre em contato pelo número: (85) 3274.7360');
+    console.log(this.contatoForm.value);
+    }else{
+      this.toastr.error('Por favor, verifique se seu e-mail está escrito corretamente ou se tem algum campo vazio.');
+    }
+  }
 }
